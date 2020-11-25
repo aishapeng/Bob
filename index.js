6968bbeefd7b9ef3f75bd1df1ca238134b31d982
aishapeng <aishaaishapeng@gmail.com>
2020-11-25 21:38:04 +0800

    yes

--- index.js
+++ index.js
@@ -20,6 +20,14 @@ admin.initializeApp({
 app.use(bodyParser.json)
 const port = process.env.PORT || 3000
 
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+}
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
 app.post('/dialogflow-fulfillment', (request, response) => {
     dialogflowFulfillment(request, response)
 });
