63594112e515c166d5bf1e1cabd42c08dbc4710f
aishapeng <aishaaishapeng@gmail.com>
2020-11-25 22:05:28 +0800

    yes

--- index.js
+++ index.js
@@ -41,12 +41,137 @@ app.listen(port, () => {
 const dialogflowFulfillment = (request, response) => {
     const agent = new WebhookClient({request, response})
 
-    function sayHello(agent){
-        agent.add("Heroku")
-    }
+    function readWeather(agent){
+        return admin.database().ref('WeatherDescribe').once('value', snapshot => {
+           const value = snapshot.val();
+              if(value !== null){
+               agent.add(`I would describe it as ${value}.`);
+              }
+            else{
+             agent.add("Oops! It seems that no value has been returned from the database");
+            }
+         });
+      }
+      
+      function readBrightness(agent){
+      return admin.database().ref('Brightness').once('value', snapshot => {
+         const value = snapshot.val();
+            if(value !== null){
+              if (value>=90){
+                 agent.add(`The brightness is ${value}%, sure is a sunny day!`);
+              }else if (value>=60){
+                agent.add(`The brightness is ${value}%, good day for gardening!`);
+              }else{
+                agent.add(`The brightness is ${value}%, sure is gloomy.`);
+              }
+            }
+          else{
+           agent.add("Uh oh, I seem to be disconnected!");
+          }
+        });
+      }
+      
+      function readHumidity(agent){
+      return admin.database().ref('Humidity').once('value', snapshot => {
+         const value = snapshot.val();
+            if(value !== null){
+            if (value>=94){
+                 agent.add(`The humidity is ${value}%, good for the okra!`);
+              }else if (value>=88){
+                agent.add(`The humidity is ${value}%, pretty decent airflow.`);
+              }else{
+                agent.add(`The humidity is ${value}%, dry air huh.`);
+              }
+            }
+          else{
+           agent.add("Oops! It seems that no value has been returned from the database");
+          }
+        });
+      }
+      
+      function readSoilMoisture(agent){
+      return admin.database().ref('SoilMoisture').once('value', snapshot => {
+         const value = snapshot.val();
+            if(value !== null){
+              if(value>=70){
+                agent.add(`The soil moisture is ${value}%, too much water!`);
+              }else if (value>=70){
+                agent.add(`The soil moisture is ${value}%, perfectly watered!`);
+              }else if (value>=30){
+                agent.add(`The soil moisture is ${value}%, I will water the plants soon.`);
+              }else{
+                agent.add(`The soil moisture is ${value}%, I will water the plants immediately!`);
+              }
+            }
+          else{
+           agent.add("Oops! It seems that no value has been returned from the database");
+          }
+        });
+      }
+      
+      function readTemperature(agent){
+      return admin.database().ref('Temperature').once('value', snapshot => {
+         const value = snapshot.val();
+            if(value !== null){
+              if (value>=35){
+                agent.add(`The temperature is ${value}%, I have to shelter teh okras!`);
+              }else if (value>=30){
+                agent.add(`The temperature is ${value}%, perfect temperature for okras.`);
+              }else{
+                agent.add(`The temperature is ${value}%, not a good day for okras.`);
+              }
+            }
+          else{
+           agent.add("Oops! It seems that no value has been returned from the database");
+          }
+        });
+      }
+      
+      function readWeatherHumidity(agent){
+      return admin.database().ref('WeatherHumidity').once('value', snapshot => {
+         const value = snapshot.val();
+            if(value !== null){
+             agent.add(`The weather humidity is ${value}`);
+            }
+          else{
+           agent.add("Oops! It seems that no value has been returned from the database");
+          }
+        });
+      }
+      
+      function readWeatherTemperature(agent){
+      return admin.database().ref('WeatherTemperature').once('value', snapshot => {
+         const value = snapshot.val();
+            if(value !== null){
+             agent.add(`The weather temperature is ${value}`);
+            }
+          else{
+           agent.add("Oops! It seems that no value has been returned from the database");
+          }
+        });
+      }
+      
+      function readWeatherWindSpeed(agent){
+      return admin.database().ref('WeatherWindSpeed').once('value', snapshot => {
+         const value = snapshot.val();
+            if(value !== null){
+             agent.add(`The wind speed is ${value}`);
+            }
+          else{
+           agent.add("Oops! It seems that no value has been returned from the database");
+          }
+        });
+      }
 
     let intentMap = new Map();
-    intentMap.set('Default Welcome Intent', sayHello)
+    intentMap.set('Weather Intent', readWeather);
+    intentMap.set('Humidity Intent', readHumidity);
+    intentMap.set('Brightness Intent', readBrightness);
+    intentMap.set('Soil Moisture Intent', readSoilMoisture);
+    intentMap.set('Temperature Intent', readTemperature);
+    intentMap.set('Weather Humidity Intent', readWeatherHumidity);
+    intentMap.set('Weather Temperature', readWeatherTemperature);
+    intentMap.set('Weather Wind Speed Intent', readWeatherWindSpeed);
 
     agent.handleRequest(intentMap)
 }
